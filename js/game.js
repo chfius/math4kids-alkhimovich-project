cd949dc8c39ac9e6395bdbf82551e880aa0b013e
Yury Alkhimovich <chfius@mail.ru>
2020-06-14 18:02:31 +0300

    make expression calc

--- js/game.js
+++ js/game.js
@@ -1,38 +1,71 @@
 'use strict';
 
 var draggedSymbol = null;
+var result = null;
 
 function symbolDragStart(EO) {
-    EO = EO || window.event;
-    draggedSymbol = EO.target;
+  EO = EO || window.event;
+  draggedSymbol = EO.target;
 }
 
 function symbolDragEnd(EO) {
-    EO = EO || window.event;
-    draggedSymbol = null;
+  EO = EO || window.event;
+  draggedSymbol = null;
 }
 
 function mathExpressionDragOver(EO) {
-    EO = EO || window.event;
-    // по-умолчанию ронять элементы в div запрещено, отменяем
-    EO.preventDefault();
+  EO = EO || window.event;
+  // по-умолчанию ронять элементы в div запрещено, отменяем
+  EO.preventDefault();
 }
 
 function mathExpressionDrop(EO, Div) {
-    // добавлен символ в формуле
-    EO = EO || window.event;
-    EO.preventDefault();
-    if (draggedSymbol)
-        Div.appendChild(draggedSymbol);
-    //если последний символ = то считаем выражение
-    if (draggedSymbol.alt == "=") evalExpr();
+  // добавлен символ в формуле
+  EO = EO || window.event;
+  EO.preventDefault();
+  if (draggedSymbol) {
+    Div.appendChild(draggedSymbol);
+  }
+  //если последний символ = то считаем выражение
+  if (draggedSymbol.alt == '=') {
+    result = evalExpr();
+  }
+  //если выражение высчитано, то смотрим ответ
+  if (result == readAnswer()) {
+    console.log('You are right!');
+  }
+}
+
+function getExpr() {
+  var expressionString = document.getElementById('math_expression');
+  var str = [];
+  expressionString.querySelectorAll('.symbol').forEach((el) => {
+    str.push(el.alt);
+  });
+  return str;
 }
 
 function evalExpr() {
-    var expressionString = document.getElementById('math_expression');
-    var str = [];
-    expressionString.querySelectorAll('.symbol').forEach((el) => { str.push(el.alt); });
-    str.pop();
-    //console.log(str.join('')); 
-    console.log(eval(str.join('')))
+  var expr = getExpr();
+  expr.pop(); //уберем в строке знак "равно"
+  return eval(expr.join(''));
+}
+
+function readAnswer() {
+  var answ = getExpr();
+  var index = answ.indexOf('=') + 1; //+1 нужен чтобы в строке не было знака =
+  return parseInt(answ.slice(index).join('').toString());
+}
+
+function numbersDragOver(EO) {
+  EO = EO || window.event;
+  EO.preventDefault();
+}
+
+function numbersDrop(EO, Div) {
+  EO = EO || window.event;
+  EO.preventDefault();
+  if (draggedSymbol) {
+    Div.appendChild(draggedSymbol);
+  }
 }
